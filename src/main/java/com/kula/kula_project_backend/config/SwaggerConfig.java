49169c44afe6a80b90694c453b7b2873a59f71ab
XYGE888 <161259337+XYGE888@users.noreply.github.com>
2024-12-10 20:52:24 +1100

    Integrated Swagger into the project.

---
+++ src/main/java/com/kula/kula_project_backend/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.kula.kula_project_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+     @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Kulaa API Documentation")
+                        .version("1.0")
+                        .description("This is all APIs for Kulaa !!!"));
+    }
+}
